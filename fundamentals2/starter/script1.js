d3027312ad7addad8f6350a0de062042f4b30667
dayakar <dayakar4122@gmail.com>
2022-07-10 20:02:12 -0500

    refactored code

--- fundamentals2/starter/script1.js
+++ fundamentals2/starter/script1.js
@@ -42,4 +42,3 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 console.log('yearsUntilRetirementLeft', yearsUntilRetirement(1991, 'daya'));
 const retirementage = yearsUntilRetirement(1991, 'daya');
 console.log('retirementage', retirementage);
-console.log('retirementage1', retirementage);
